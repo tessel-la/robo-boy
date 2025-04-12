07357ab80905ea9e0bc0be043120e9c5b3b5c589
ricfrr <riccardo.franceschini@live.it>
2025-04-12 20:28:05 +0200

    style to horizonal bar included

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.f3tt2cm02jg"
+    "revision": "0.um9a1a696t8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
