96873e278df812281976205ffb78982919c77bda
ricfrr <riccardo.franceschini@live.it>
2025-04-29 20:55:01 +0200

    fixed dropdwon vertical disalignment

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.cuo9p62ujpg"
+    "revision": "0.2qaek0o64l8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
