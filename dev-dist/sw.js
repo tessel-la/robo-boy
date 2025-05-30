b4543bccba5995d79ff1223edcba6957728c7a5a
ricfrr <riccardo.franceschini@live.it>
2025-05-30 19:37:24 +0200

    fixed arrow positioning when resizing

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.mr9ft616qco"
+    "revision": "0.50qb9r207mo"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
