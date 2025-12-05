5c995a8e26c0c5b95b69cf7b39558e1e0fff6532
ricfrr <riccardo.franceschini@live.it>
2025-12-05 19:43:21 +0100

    removed old ros3d implementation that was dangling from an old implementation

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-54d0af47'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.6t0ml03e15"
+    "revision": "0.tevcihjqf0g"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
