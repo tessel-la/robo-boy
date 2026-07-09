d0418fb45a47291e03f7cb13e6056a8e43c2c2f6
ricfrr <riccardo.franceschini@live.it>
2026-07-09 09:03:08 +0200

    ui consistency improv

--- config/vite.config.ts
+++ config/vite.config.ts
@@ -5,6 +5,7 @@ import { VitePWA } from 'vite-plugin-pwa';
 
 // https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
+  base: mode === 'tauri' ? './' : '/',
   server: {
     host: '0.0.0.0', // Listen on all interfaces within the container
     port: 5173,
